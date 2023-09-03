dd48a69b4b1b3902d9d90b26d7ada06a7f59fb0a
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-09-03 16:16:32 -0400

    Added API routes, and controllers

---
+++ models/routes/api/userRoutes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController.js');
+
+router.route ('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser);
+
+// PUT to update a user by its _id
+router.route('/:userId').put(updateUser);
+
+// DELETE to remove user by its _id
+router.route('/:userId').delete(deleteUser);
+
+// BONUS: Remove a user's associated thoughts when deleted.
+router.route('/:userId').delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+// POST to add a new friend to a user's friend list
+router.route('/:userId/friends/:friendId').post(addFriend);
+
+// DELETE to remove a friend from a user's friend list
+router.route('/:userId/friends/:friendId').delete(deleteFriend);
+
+
+module.exports = router;
+
+
+
