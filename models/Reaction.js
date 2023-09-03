46996b0f34e56cef1b6d05567c493e66beea305a
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-09-03 15:47:15 -0400

    Initial Commit with Models

---
+++ models/Reaction.js
@@ -0,0 +1,33 @@
+const { Schema } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment's _id field
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // Use a getter method to format the timestamp on query
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+module.exports = reactionSchema;
