e13dbed1154ffdd3f88104f4362939694862a356
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-09-04 16:42:34 -0400

    Changed connection config

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,5 @@
 const { connect, connection } = require('mongoose');
-const serverName = 'bdaBubbleUp'
 
-connect(`mongodb://127.0.0.1:27017/${serverName}`);
+connect('mongodb://127.0.0.1:27017/bdaBubbleUp');
 
 module.exports = connection;
